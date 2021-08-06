8c4b8e52e8c8f1922236ff777e054d7cc9cf0d59
Woondeling Colina <josecolinamartinez@gmail.com>
2021-08-06 01:51:09 -0500

    eliminando componente que no se usa

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { BrowserRouter,
         Route, 
-        Switch, 
-        Link
+        Switch
     } from 'react-router-dom'
 
 import Navbar from '../components/Navbar';
